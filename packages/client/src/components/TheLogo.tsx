793009520a60756911cbd900676e5b00837d30a9
Jorge Vargas <jorgevargasla@gmail.com>
2023-01-30 17:52:49 -0500

    style: add link to home page in logo

--- packages/client/src/components/TheLogo.tsx
+++ packages/client/src/components/TheLogo.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function TheLogo() {
   return (
     <figure className="logo">
-      <img
-        srcSet="/images/Logo_ML.png, /images/Logo_ML@2x.png 2x"
-        src="/images/Logo_ML.png"
-        alt="MercadoLibre Logo"
-      />
+      <Link to="/">
+        <img
+          srcSet="/images/Logo_ML.png, /images/Logo_ML@2x.png 2x"
+          src="/images/Logo_ML.png"
+          alt="MercadoLibre Logo"
+        />
+      </Link>
     </figure>
   );
 }

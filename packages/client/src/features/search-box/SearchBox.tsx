fa5e189274019817d6d7c33dfaf23e7627707c78
Jorge Vargas <jorgevargasla@gmail.com>
2023-01-30 17:53:33 -0500

    feat: navigate to search results

--- packages/client/src/features/search-box/SearchBox.tsx
+++ packages/client/src/features/search-box/SearchBox.tsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import SearchBoxInput from "./components/SearchInput";
 import "./SearchBox.scss";
 
 export default function SearchBox() {
+  const navigate = useNavigate();
+
+  const handleSearch = (value: string) => {
+    if (value) {
+      navigate(`/items?search=${value}`);
+    }
+  };
+
   return (
     <div className="search-box">
-      <SearchBoxInput />
+      <SearchBoxInput onSearch={handleSearch} />
     </div>
   );
 }
